46ddb0cae5640dbb6ecf3a27a4c70b14410d7127
Lois Di Qual <lois@timber.so>
2020-01-13 14:44:34 -0800

    Sources

---
+++ Sources/SwiftUIRefresh.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUIRefresh.h
+//  SwiftUIRefresh
+//
+//  Created by Lois Di Qual on 1/13/20.
+//  Copyright © 2020 Siteline. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIRefresh.
+FOUNDATION_EXPORT double SwiftUIRefreshVersionNumber;
+
+//! Project version string for SwiftUIRefresh.
+FOUNDATION_EXPORT const unsigned char SwiftUIRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIRefresh/PublicHeader.h>
+
+
